5b2e37524c05669da72c30cc8147b246ea210ae4
Tatikonda Divya Kiran <79193670+DivyaKiran2001@users.noreply.github.com>
2024-09-16 15:42:16 +0530

    Update tdz.js

--- tdz.js
+++ tdz.js
@@ -1,1 +1,10 @@
 // let and const in JS and Temporal dead zone
+
+// TDZ(Temporal Dead Zone) - TDZ is the period where the variables were not accessible until
+// the moment the computer initializes it with a value.
+
+console.log(a);
+
+let a=10;
+var b=100;
+console.log(b);
